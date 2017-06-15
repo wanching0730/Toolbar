8fe2a8e914ed0a8fb237d45141dc0fa5935d1783
Wan Ching <wanching_97@hotmail.com>
2017-06-15 14:34:16 +0800

    git add

---
+++ app/src/main/java/com/example/android/toolbar/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.android.toolbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_setting:
+                return true;
+            case R.id.action_favourite:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
